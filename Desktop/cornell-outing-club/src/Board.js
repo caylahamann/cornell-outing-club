6bb092dd634641a967e51321f9190f362bfa38e1
Cayla Hamann <ch629@cornell.edu>
2018-09-03 19:36:54 -0400

    first draft

--- Desktop/cornell-outing-club/src/Board.js
+++ Desktop/cornell-outing-club/src/Board.js
@@ -17,7 +17,7 @@ class Board extends Component {
                 <hr/>
                 <div className="cards flex flex-wrap justify-center">
                     <Card name={'Anya Sherman'} 
-                    title={'president'} 
+                    title={'co-president'} 
                     image={'AnyaSherman.jpg'}
                     description={"I love running, climbing, backpacking, kayaking and hammocking.  I'm an Outdoor Odyssey Guide, as well as teaching rock climbing for COE, and am very involved with Cornell Running Club."}
                     major={"Environmental Engineering"}
@@ -47,22 +47,6 @@ class Board extends Component {
                     major={"Environmental Engineering"}
                     year={"2021"}
                     funfact={"Hadi knows no fun"}
-                    />
-                    <Card name={'Bram Wallace'} 
-                    title={'Backpacking/Hiking Chair'} 
-                    image={'BramWallace.jpg'}
-                    description={"Hiking, trail running, backpacking, kayaking, surfing, literally anything"}
-                    major={"Applied Math"}
-                    year={"2nd year PhD"}
-                    funfact={"I've been growled at by a mama mountain lion standing over her 3 cubs"}
-                    />
-                    <Card name={'Cat Ly'} 
-                    title={'Backpacking/Hiking Trip Leader'} 
-                    image={'CatLy.jpg'}
-                    description={"hugging trees, backpacking and hiking, nature and landscape photography, environmental sustainability; Cornell Outdoor Education instructor"}
-                    major={"Chemistry"}
-                    year={"2019"}
-                    funfact={"Years and years ago, at a Buddhist temple atop a mountain in foggy California, I pet a wolf that was living among the temple monks. I relate to her on a spiritual level. Her name was Mimi, and she was quite the lone wolf; occasionally, she would visit the peaceful monks of the temple and walk alongside them. Someday, I hope to live a life as diverse and exciting as Mimi's as an Icelandic hermit."}
                     />
                     <Card name={'Jackson Siff'} 
                     title={'Trip Leader'} 
